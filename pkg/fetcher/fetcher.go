0c78c74ec7c044c1742344026080e9e64790db98
Enzo <admin@tcp.so>
2022-05-13 21:08:24 +0800

    add http代理

--- pkg/fetcher/fetcher.go
+++ pkg/fetcher/fetcher.go
@@ -9,18 +9,27 @@ import (
 	"io/ioutil"
 	"log"
 	"net/http"
+	"net/url"
 )
 
 // Fetch 用于获取网页内容
-func Fetch(url string) ([]byte, error) {
-	client := &http.Client{}
-	request, err := http.NewRequest(http.MethodGet, url, nil)
+func Fetch(u string) ([]byte, error) {
+	// HTTP 代理
+	proxy := func(_ *http.Request) (*url.URL, error) {
+		return url.Parse("http://127.0.0.1:1080")
+	}
+	transport := &http.Transport{Proxy: proxy}
+	client := &http.Client{Transport: transport}
+	request, err := http.NewRequest(http.MethodGet, u, nil)
 	if err != nil {
 		return nil, err
 	}
 	request.Header.Set("Referer", "https://www.gutenberg.org/")
 	request.Header.Set("User-Agent", "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/100.0.4896.127 Safari/537.36")
 	resp, err := client.Do(request)
+	if err != nil {
+		return nil, err
+	}
 	defer resp.Body.Close()
 	bodyReader := bufio.NewReader(resp.Body)
 	e := determineEncoding(bodyReader)
