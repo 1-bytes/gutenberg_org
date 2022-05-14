d4c124aa95d781b5e259793442964e0b2fe6d7eb
Enzo <admin@tcp.so>
2022-05-14 13:43:41 +0800

    增加数据库存储

--- cmd/cmd.go
+++ cmd/cmd.go
@@ -1,1 +1,93 @@
 package cmd
+
+import (
+	"encoding/json"
+	"golang.org/x/net/context"
+	"log"
+	"spider/bootstrap"
+	"spider/model"
+	"spider/parser"
+	"spider/pkg/elastic"
+	"strconv"
+	"strings"
+)
+
+func SavaData(bookDetail *parser.Parser) {
+	db := bootstrap.DB
+	splitParagraphs := SplitParagraph(&bookDetail.Content)
+	// 存储至 MySQL
+	articleModel := model.DictArticle{
+		Type:                "1",
+		Title:               bookDetail.Title,
+		Author:              bookDetail.Author,
+		CoverImage:          bookDetail.CoverImage,
+		Category:            "0",
+		DownloadCount:       bookDetail.DownloadCount,
+		ReleaseDate:         bookDetail.ReleaseDate,
+		MostRecentlyUpdated: "",
+		SourceDomain:        "5",
+		Status:              "1",
+		Rank:                99,
+	}
+	db.Table(articleModel.TableName()).Create(&articleModel)
+	// 存储至 ElasticSearch
+	for _, paragraph := range splitParagraphs {
+		articleID := int(articleModel.ID)
+		data := parser.ElasticSearchData{
+			ID:           strconv.Itoa(articleID),
+			SourceDomain: "www.gutenberg.org",
+			SourceUrl:    bookDetail.URL,
+			Paragraph: struct {
+				EN string `json:"en"`
+			}{
+				EN: paragraph,
+			},
+		}
+		dataJson, err := json.Marshal(data)
+		if err != nil {
+			log.Printf("error: elasticsearch json marshal failed: %s", err)
+			continue
+		}
+
+		client := elastic.GetInstance().Index()
+		do, err := client.
+			Index("dict_article_test").
+			BodyJson(string(dataJson)).
+			Timeout("10s").
+			Do(context.Background())
+		if err != nil {
+			log.Printf("error: save data to the elasticsearch failed: %s", err)
+			continue
+		}
+		// 存储至 MySQL
+		articleParagraph := model.DictArticleParagraph{
+			ElasticID:     do.Id,
+			ArticleID:     articleID,
+			ByteCount:     articleModel.DownloadCount,
+			Fre:           "",
+			Fkgl:          "",
+			SchoolLvClass: 0,
+			SchoolLvlName: "",
+			TechWordLv:    "",
+			CefrWordLv:    "",
+			Status:        1,
+		}
+		db.Table(articleParagraph.TableName()).Create(&articleParagraph)
+	}
+}
+
+// SplitParagraph 段落拆分
+func SplitParagraph(resp *string) []string {
+	paragraphs := strings.Split(*resp, "\r\n\r\n")
+	var result []string
+	for _, paragraph := range paragraphs {
+		paragraph = strings.ReplaceAll(paragraph, "\r\n", " ")
+		paragraph = strings.TrimSpace(paragraph)
+		paragraph = strings.Trim(paragraph, "***")
+		if paragraph == "" {
+			continue
+		}
+		result = append(result, paragraph)
+	}
+	return result
+}
--- main.go
+++ main.go
@@ -11,26 +11,28 @@ import (
 )
 
 func main() {
+	bootstrap.Setup()
 	baseU := "https://www.gutenberg.org/ebooks/"
 	//for i := 1; i < 68100; i++ {
-	for i := 67998; i < 67999; i++ {
+	for i := 8; i < 9; i++ {
 		u := baseU + strconv.Itoa(i)
 		fmt.Printf("request url: %s\n", u)
-		bytes, err := fetcher.Fetch(u)
+		resp, err := fetcher.Fetch(u)
 		if err != nil {
-			log.Printf("error: send request faild: %s", err)
+			log.Printf("error: send request failed: %s", err)
 			continue
 		}
 		p := parser.Parser{}
-		p.GetDetail(bytes)
+		p.GetDetail(u, resp)
+		cmd.SavaData(&p)
 
-		fmt.Println(p.Title)
-		fmt.Println(p.Author)
-		fmt.Println(p.Language)
-		fmt.Println(p.ReleaseDate)
-		fmt.Println(p.DownloadCount)
-		fmt.Println(p.CoverImage)
-		fmt.Println(p.Content)
-		fmt.Println("\n")
+		//fmt.Println(p.Title)
+		//fmt.Println(p.Author)
+		//fmt.Println(p.Language)
+		//fmt.Println(p.ReleaseDate)
+		//fmt.Println(p.DownloadCount)
+		//fmt.Println(p.CoverImage)
+		//fmt.Println(p.Content)
+		//fmt.Println("\n")
 	}
 }
